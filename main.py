2d59a54b01dcfa0e796ed4a6977e04b1e5b586e2
Jithin A P <jithinap009@gmail.com>
2020-09-18 08:50:46 +0530

    Fixed

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from flask import Flask
 from uuid import uuid4
-from .blockchain import Blockchain
-
+from blockchain import Blockchain
+import json
 
 app = Flask(__name__)
 node_identifier = str(uuid4()).replace('-', '')
 
 blockchain = Blockchain()
 
-@app.route('mine/', methods = ['GET'])
+@app.route('/mine', methods = ['GET'])
 def mine():
     return 'Mining'
 
-@app.route('transactions/new', methods=['POST'])
+@app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    return "Trasacion"
+    return "Trasaction"
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
@@ -22,7 +22,7 @@ def full_chain():
         'chain': blockchain.chain,
         'length': len(blockchain.chain)
     }
-    return jaisonify(response), 200
+    return jsonify(response), 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
