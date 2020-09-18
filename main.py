0d1b30fb1e09ae26e39b2623f112b537377b3320
Jithin A P <jithinap009@gmail.com>
2020-09-18 08:50:30 +0530

    Flask server added

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 from flask import Flask
 from uuid import uuid4
+from .blockchain import Blockchain
 
 
 app = Flask(__name__)
-node_identifier = 
+node_identifier = str(uuid4()).replace('-', '')
+
+blockchain = Blockchain()
+
+@app.route('mine/', methods = ['GET'])
+def mine():
+    return 'Mining'
+
+@app.route('transactions/new', methods=['POST'])
+def new_transaction():
+    return "Trasacion"
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+    return jaisonify(response), 200
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
